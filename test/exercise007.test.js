643a19960e97951c095c905297c58a1a7f816412
AlieuBSagnia <alieubsagnia@gmail.com>
2021-03-17 21:57:48 +0000

    Exercise 7 sumDigits Function & Test complete

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -7,6 +7,12 @@ const {
 } = require('../challenges/exercise007');
 
 describe("sumDigits", () => {
+
+    test("returns the sum of all the digits from given array", () => {
+        expect(sumDigits([4, 6, 3])).toBe(13);
+        expect(sumDigits([55, 62, 10])).toBe(127);
+        expect(sumDigits([28, 22, 100])).toBe(150);
+      });
     
 });
 
